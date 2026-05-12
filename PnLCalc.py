61701a04498a31b5f96996a4275679368314872c
jbarosa <joaodanielbarosa@gmail.com>
2026-05-12 08:45:24 +0100

    feat: add put credit spread pnl

--- PnLCalc.py
+++ PnLCalc.py
@@ -7,30 +7,14 @@ def put_credit_spread_pnl(participation, premium_received, K1, K2, S):
     # PnL = Premium received + Payoff at maturity
     payoff = max(0, K1 - S) - max(0, K2 - S)
     
-    current_pnl = premium_received + payoff * participation
+    current_pnl = premium_received + payoff * participation*100  # PnL in euros, considering participation and contract size (100)
     max_profit = premium_received  # Max profit occurs if both options expire worthless
-    max_loss = (K2 - K1) * participation - premium_received  # Max loss occurs if S <= K1
-    
+    max_loss = (K2 - K1) * participation*100  # Max margin is the maximum loss potential of the spread
+    max_cover = K2 * participation*100  # Max cover is the maximum amount that can be lost if the underlying price drops to ~k2, which is the strike of the long put times participation and contract size  
     #k1 < S < k2 -> payoff = 0 - max(0, K2 - S) = -(K2 - S) -> loss but limited
+   
     
-    
-    return current_pnl, max_profit, max_loss
+    return current_pnl, max_profit, max_loss, max_cover
 
 
-# Example usage of put credit spread payoff and price
-spot=16
 
-put2_strike = 14
-put2_bid = 0.11
-put2_ask = 0.38
-
-put1_strike = 13.5
-put1_bid = 0.04
-put1_ask = 0.7
-
-participation = 1  # Number of spreads
-premium_received = put2_bid - put1_ask  # Net premium received from selling the spread
-current_liquidation = premium_received - put2_ask + put1_bid # Current liquidation value if we were to close the position now (buy back the short put at ask, sell the long put at bid)
-
-print(f"Premium received: {participation*premium_received}")
-print(f"Current liquidation value: {participation*current_liquidation}")
--- worksheet.py
+++ worksheet.py
@@ -1,5 +1,6 @@
 import StructuredProducts as sp
 import DerivativesStrategies as ds
+import PnLCalc as pnl
 
 def price_cp(S, K, T, r, q, sigma, participation, protection, cap=0):
     return sp.capital_protection_price(S, K, T, r, q, sigma, participation, protection, cap)
@@ -7,3 +8,40 @@ def price_cp(S, K, T, r, q, sigma, participation, protection, cap=0):
 cp_silver = price_cp(79.7961,63,2,0.03,0,0.2,1,90,cap=160)
 print(cp_silver)
 
+s=12.0
+k1=10.5
+k2=11.0
+participation=4
+t=30
+k1_bid=0.04
+k1_ask=0.05
+k2_bid=0.10
+k2_ask=0.11
+
+premium_received_test = (k2_bid - k1_ask)*participation*100  # Net premium received from selling the spread
+print(f"Premium received: {premium_received_test}€")
+
+current_pnl, max_profit, max_loss, max_cover = pnl.put_credit_spread_pnl(participation, premium_received_test, k1, k2, s)
+print(f"Current PnL: {current_pnl}€")
+print(f"Max margin required: {max_loss}€")
+print(f"Max profit: {max_profit}€")
+print(f"Max cover: {max_cover}€")
+
+s=739
+k1=670
+k2=675
+participation=1
+t=30
+k1_bid=1.65
+k1_ask=1.67
+k2_bid=1.86
+k2_ask=1.88
+
+premium_received_test = (k2_bid - k1_ask)*participation*100  # Net premium received from selling the spread
+print(f"Premium received: {premium_received_test}€")
+
+current_pnl, max_profit, max_loss, max_cover = pnl.put_credit_spread_pnl(participation, premium_received_test, k1, k2, s)
+print(f"Current PnL: {current_pnl}€")
+print(f"Max margin required: {max_loss}€")
+print(f"Max profit: {max_profit}€")
+print(f"Max cover: {max_cover}€")
