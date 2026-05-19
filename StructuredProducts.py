393a433f9a08512ea0fb2ccc2083f31eddfcb613
jbarosa <you@example.com>
2026-05-19 17:03:44 +0100

    feat: add BRC payoff and price

--- StructuredProducts.py
+++ StructuredProducts.py
@@ -4,8 +4,8 @@ from Derivatives import black_scholes_call, black_scholes_put
 # Payoff and price functions for structured products:
     # Capital Protection: long call option + long zero-coupon bond
     # Discount certificate: long stock + short call option or bond + short put option
-    # Reverse convertible: bond + coupons + short put option
-    # Counditional Coupon Barrier Reverse Convertible: bond + short put option with barrier
+    # Reverse convertible: bond + short put option
+    # Counditional Coupon Barrier Reverse Convertible: bond + coupons + short put option with barrier (down-and-in put)
     # Autocallable:
     # Tracker certificate:
     # Outperformance certificate:
@@ -51,3 +51,17 @@ def reverse_convertible_price(S, K, T, r, q, sigma, participation, notional, cou
     put_price = black_scholes_put(S, K, T, r, q, sigma)
     zero_cupon_bond_price = notional * (1 + r) ** (-T)
     return zero_cupon_bond_price + coupon * (1 - (1 + r) ** (-T)) / r - put_price * participation  # Price of the bond plus the present value of coupons minus the price of the short put option
+
+def barrier_reverse_convertible_payoff(S, K, H, T, r, q, sigma, participation, notional, coupon):
+    #bond + coupons + short put option with barrier (down-and-in put)
+    if S <= H:
+        payoff = notional + coupon - participation * max(0, K - S)# payoff = S+coupon if K=notional  # Payoff from the bond plus coupons minus the payoff from the short put option
+    else:
+        payoff = notional + coupon  # Payoff from the bond plus coupons (short put option expires worthless)
+    return payoff
+
+def barrier_reverse_convertible_price(S, K, H, T, r, q, sigma, participation, notional, coupon):
+    #bond + coupons + short put option with barrier (down-and-in put)
+    down_and_in_put_price = black_scholes_put(S, K, T, r, q, sigma) - black_scholes_put(S, K, T, r, q, sigma) * (H / S) ** (2 * (r - q + 0.5 * sigma**2) / sigma**2)
+    zero_cupon_bond_price = notional * (1 + r) ** (-T)
+    return zero_cupon_bond_price + coupon * (1 - (1 + r) ** (-T)) / r - down_and_in_put_price * participation  # Price of the bond plus the present value of coupons minus the price of the short put option with barrier
